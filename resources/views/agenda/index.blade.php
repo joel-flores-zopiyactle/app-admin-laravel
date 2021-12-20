d899c85cca43fe6a49fc58d839ee05179c48e11b
joel-flores-zopiyactle <joelfloreszopiyactle@gmail.com>
2021-12-20 15:11:25 -0600

    add librerias

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -1,8 +1,34 @@
+
 @extends('layouts.dashboard')
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/agenda.css') }}">
+@endsection
 
 @section('content')
-<div class="container-fluid">
-    
-    agenda
+<div class="container-fluid mb-5">
+    <div class="shadow p-2 card bg-white">
+        <div id='calendar'></div>
+    </div>
 </div>
+@endsection
+
+@section('js')
+    <script src="{{ asset('js/agenda.js') }}"></script>
+    <script src="{{ asset('js/agenda-es.js') }}"></script>
+
+    <script>
+         document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                locale: 'es',
+                headerToolbar: {
+                    start: 'title', 
+                    center: '',
+                    end: 'today prev,next'
+                }
+            });
+            calendar.render();
+        });
+    </script>
 @endsection
