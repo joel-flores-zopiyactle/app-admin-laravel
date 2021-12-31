631552101445eb15fff6f4dd1fba99fe814a3d1c
joel-flores-zopiyactle <joelfloreszopiyactle@gmail.com>
2021-12-30 23:51:40 -0600

    dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -38,7 +38,7 @@
   </head>
   <body>
     
-    <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 text-center" href="#">
             <img class="bg-white" style="width: 80px;" src="{{ asset('img/sinjo_logo.png') }}" alt="" srcset="">
         </a>
