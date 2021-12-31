1307796c149ac3b83d77797919fd8c8f74f2d43d
joel-flores-zopiyactle <joelfloreszopiyactle@gmail.com>
2021-12-31 00:20:07 -0600

    diseño de formularios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <div class="col-md-4">
+            <div class="card p-2 rounded shadow">
+                <div class="h4 text-center p-3">{{ __('Login') }} <hr></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
+                                <label for="email" class="col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -26,9 +26,8 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
+                            <div class="col-md-12">
+                                <label for="password" class="col-form-label text-md-right">{{ __('Password') }}</label>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -40,7 +39,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-12">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -51,17 +50,17 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                        <div class="row mb-5">
+                            <div class="col-md-12 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
+                                {{-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
+                                @endif --}}
                             </div>
                         </div>
                     </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,19 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+        <div class="col-md-5">
+            <div class="card shadow rounded p-3">
+                <div class="h4 text-center">{{ __('Register') }} <hr> </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                         @csrf
 
            
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-            
-                            <div class="col-md-6">
+                        <div class="row mb-3">            
+                            <div class="col-md-12">
+                                <label for="name" class="col-form-label text-md-right">{{ __('Name') }}</label>
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
             
                                 @error('name')
@@ -26,10 +25,9 @@
                             </div>
                         </div>
             
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-            
-                            <div class="col-md-6">
+                        <div class="row mb-3">            
+                            <div class="col-md-12">
+                                <label for="email" class="col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
             
                                 @error('email')
@@ -41,11 +39,10 @@
                         </div>
             
             
-                        <div class="row mb-3">
-                            <label for="telefono" class="col-md-4 col-form-label text-md-right">{{ __('Telefono') }}</label>
-            
-                            <div class="col-md-6">
-                                <input id="telefono" type="number" class="form-control @error('telefono') is-invalid @enderror" name="telefono" value="{{ old('telefono') }}" required autocomplete="telefono">
+                        <div class="row mb-3">            
+                            <div class="col-md-12">
+                                <label for="telefono" class="col-form-label text-md-right">{{ __('Telefono') }}</label>
+                                <input id="telefono" type="number" class="form-control @error('telefono') is-invalid @enderror" name="telefono" value="{{ old('telefono') }}" min="10" max="10" required autocomplete="telefono">
             
                                 @error('telefono')
                                     <span class="invalid-feedback" role="alert">
@@ -55,10 +52,9 @@
                             </div>
                         </div>
             
-                       {{--  <div class="row mb-3">
-                            <label for="avatar" class="col-md-4 col-form-label text-md-right">{{ __('Avatar') }}</label>
-            
-                            <div class="col-md-6">
+                       {{--  <div class="row mb-3">           
+                            <div class="col-md-12">
+                                 <label for="avatar" class="col-form-label text-md-right">{{ __('Avatar') }}</label>
                                 <input id="avatar" type="file" class="form-control @error('avatar') is-invalid @enderror" name="avatar" value="{{ old('avatar') }}" required autocomplete="avatar">
             
                                 @error('avatar')
@@ -69,10 +65,9 @@
                             </div>
                         </div> --}}
             
-                        <div class="row mb-3">
-                            <label for="avatar" class="col-md-4 col-form-label text-md-right">{{ __('Tipo de usuario') }}</label>
-            
-                            <div class="col-md-6">
+                        <div class="row mb-3">            
+                            <div class="col-md-12">
+                                <label for="avatar" class="col-form-label text-md-right">{{ __('Tipo de usuario') }}</label>
                                 <select class="form-control" name="tipo_usuario_id" id="tipo_usuario_id">
                                     <option value="1">Admin</option>
                                 </select>
@@ -86,10 +81,9 @@
                         </div>
             
             
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-            
-                            <div class="col-md-6">
+                        <div class="row mb-3">            
+                            <div class="col-md-12">
+                                <label for="password" class="col-form-label text-md-right">{{ __('Password') }}</label>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
             
                                 @error('password')
@@ -100,10 +94,9 @@
                             </div>
                         </div>
             
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-            
-                            <div class="col-md-6">
+                        <div class="row mb-3">            
+                            <div class="col-md-12">
+                                <label for="password-confirm" class="col-form-label text-md-right">{{ __('Confirm Password') }}</label>
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
